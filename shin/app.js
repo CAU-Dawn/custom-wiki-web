d281346633a0203f8420a45a9f2a8341a586eb14
Shin <advanwith@gmail.com>
2018-02-20 22:24:29 +0900

    db upadte function
    next : 초기로딩시 db data render

--- shin/app.js
+++ shin/app.js
@@ -16,9 +16,8 @@ db.once('open', function(){
 });
 mongoose.connect('mongodb://localhost:27017');
 
-app.post('/index.html', function(req, res) {
+/*app.post('/index.html', function(req, res) {
     console.log(req.body);
-    //생성
     var wiki = new Wikidb();
     wiki.title = req.body.title;
     //wiki.author = req.body.author;
@@ -35,17 +34,33 @@ app.post('/index.html', function(req, res) {
         res.json({status: 1});
         console.log({status: 1});
     });
+});*/
 
+app.post('/', function(req, res){ //라우팅이 여러가지에서 문제가 생겨..
+    //res.render('index.html'); db datd를 로드시킬 하나의 방법?
+    //아니면 아예 html 로드 방식을 바꿔버려?
+    var title = req.body.title;
+    var contents = req.body.contents;
+    var date = new Date();
+    Wikidb.findOne({'title':title},function(err,wiki){
+        if(err){
+           console.log(err);
+           res.status(500).send('update error');
+           return;
+        }
+       wiki.data = contents;
+       wiki.edited_date = date;
+       wiki.save(function(err,silence){
+              if(err){
+                 console.log(err);
+                 res.json({status: 0});
+                 return;
+              }
+              console.log(wiki);
+              res.json({status: 1});
+          });
+      });
 });
-//검색
-/*Wikidb.find(function(err, wikis){
-    if(err) return res.status(500).send({error: 'database failure'});
-    res.json(wikis);
-})*/
-
-
 http.createServer(app).listen(3000, function(){
     console.log('Express 서버가 3000번 포트에서 시작됨');
 });
-
-// require : routing 작업(path들을 변수로 만들기), 모듈화 작업
