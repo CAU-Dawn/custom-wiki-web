c5c8b98d6a67bf43dbec454348aad2fe5911b485
Catnap <wlq7410@naver.com>
2018-04-18 18:59:12 +0900

    4.18 -1
    1. 404 에러 핸들러 추가

--- app.js
+++ app.js
@@ -29,6 +29,10 @@ app.use('/', index);
 
 app.use('/backdoor', backdoor); // backdoor로 들어온 라우팅 관리
 
+app.use(function(req, res, next) {
+    res.status(404).send('Sorry cant find that!');
+  });
+
 app.use(function(err, req, res, next) {
     console.error(err.stack);
     res.status(500).send('Error!');
